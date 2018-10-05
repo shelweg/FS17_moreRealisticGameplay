4150822f394a5dd7510c72d0bb0952f7e8dfb0b8
Stefan Helweg <8866579+shelweg@users.noreply.github.com>
2018-10-05 10:21:26 +0200

    Upload dural's released Version 1.1.0.3

---
+++ OverrideWashable.lua
@@ -0,0 +1,15 @@
+﻿
+--**************************************************************************************
+-- make use of the gameplay dirt multiplier setting
+Washable.mrGetIntervalMultiplier = function(superFunc)
+    if g_currentMission.missionInfo.dirtInterval == 1 then
+        return 0
+    elseif g_currentMission.missionInfo.dirtInterval == 2 then
+        return 0.25 * g_currentMission.mrGameplayDirtSpeedMultiplier
+    elseif g_currentMission.missionInfo.dirtInterval == 3 then
+        return 0.5 * g_currentMission.mrGameplayDirtSpeedMultiplier
+    elseif g_currentMission.missionInfo.dirtInterval == 4 then
+        return 1 * g_currentMission.mrGameplayDirtSpeedMultiplier
+    end
+end
+Washable.getIntervalMultiplier = Utils.overwrittenFunction(Washable.getIntervalMultiplier, Washable.mrGetIntervalMultiplier)
